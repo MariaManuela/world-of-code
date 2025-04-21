82514c27fd4856161678c34c9acddcd8a228d0c4
MariaManuela <patrumariamanuela@gmail.com>
2025-04-22 01:19:50 +0300

    create next app and import map images

---
+++ components/WorldMap.tsx
@@ -0,0 +1,12 @@
+// components/WorldMap.tsx
+import React from "react"; // Always import React if you're using JSX
+
+export default function WorldMap() {
+  return (
+    <div>
+      <img src="/dessert.png" />
+      <img src="/forest.png" />
+      <img src="/mountains.png" />
+    </div>
+  );
+}
